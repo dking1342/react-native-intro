04a265136f12fc1a1cfad3b19281158a16944d10
David <leanmachine42@yahoo.com>
2022-06-19 21:11:44 -0500

    review text inputs

--- App.tsx
+++ App.tsx
@@ -1,52 +1,55 @@
-import React, { useState } from 'react'
-import {
-  Button,
-  StyleSheet,
-  Text, View
-} from 'react-native';
+import React, {useState} from 'react';
+import {Button, StyleSheet, Text, TextInput, View} from 'react-native';
 
 const App = () => {
-  const [name, setName] = useState("kavooce");
-  const [person, setPerson] = useState({ name: "mario", age: 30 });
+  const [name, setName] = useState<string>('kavooce');
+  const [age, setAge] = useState<number>(30);
 
-  const handlePressText = () => {
-    setName("cat")
-  }
-
-  const handlePressObject = () => {
-    setPerson({ name: "luigi", age: 40});
-  }
+  const handleChange = (val: string, type:"name" | "age") => {
+    if(type === "name"){
+      setName(val);
+    }
+    if(type === "age"){
+      setAge(Number(val));
+    }
+  };
 
   return (
     <View style={styles.container}>
-      <Text>My name is { name }</Text>
-      <Text>His name is {person.name} and his age is {person.age}</Text>
-      <View style={styles.buttonContainer}>
-        <Button title="update text" color={"white"} onPress={handlePressText} />
-      </View>
-      <View style={styles.buttonContainer}>
-        <Button title="update object" color={"white"} onPress={handlePressObject} />
-      </View>
+      <Text>Enter name:</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="ex John Doe"
+        onChangeText={val => handleChange(val,"name")}
+      />
+      <Text>Enter age:</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="ex 30"
+        onChangeText={val => handleChange(val,"age")}
+        keyboardType="numeric"
+      />
+      <Text>
+        name: {name || "Mario"} age: {age || 30}
+      </Text>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex:1,
-    backgroundColor:"#fff",
-    alignItems:"center",
-    justifyContent:"center",
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-  buttonContainer: {
-    marginTop:20,
-    backgroundColor:"#000",
-    borderRadius:25,
-    padding:10,
-    display:"flex",
-    width:"90%",
+  input: {
+    borderWidth: 1,
+    borderColor: '#777',
+    padding: 8,
+    margin: 10,
+    width: 200,
   },
-  
 });
 
 export default App;
